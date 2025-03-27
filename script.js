bd8b4983d4a47ba7c1c37a960697f2fceed1ece7
lohithpuvvala <lohithpuvvala@gmail.com>
2025-03-27 11:54:58 +0530

    Updated toggleVideoStatus, updatePlayIcon & stopVideo functions

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const video = document.getElementById('video');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
+const playIcon = document.getElementById('play');
+const stopIcon = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+
+//Play & pause video
+function toggleVideoStatus(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
+
+// update play/pause icon
+function updatePlayIcon(){
+    if(video.paused)
+    {
+        playIcon.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    }else{
+        playIcon.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+//Update progress & timestamp
+function updateProgress(){
+    return true;
+}
+
+//Set Video time to progress.
+function setVideoProgress(){
+    return true;
+}
+
+//Stop Video
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
 
 //Event Listeners
 video.addEventListener('click', toggleVideoStatus);
@@ -10,6 +45,8 @@ video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
 
-play.addEventListener('click',toggleVideoStatus);
+playIcon.addEventListener('click',toggleVideoStatus);
 
-stop.addEventListener('click',toggleVideoStatus);
+stopIcon.addEventListener('click',stopVideo);
+
+progress.addEventListener('change',setVideoProgress);
